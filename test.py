2fbfe03167c7e190c469cbeee22a04e2d50d5671
UMAPATHYGURAJALA <umapathy.gurajala@socgen.com>
2024-10-09 15:29:16 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,9 @@
 import base64
 
+
 def encode_string(mystring):
     return base64.b64encode(mystring).decode("ascii")
 
+
 def decode_string(mystring_bytes):
     return base64.b64decode(mystring_bytes).decode("ascii")
-
-
-
